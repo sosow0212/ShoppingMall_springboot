2b870d02f2273f7c1fb914fa9bbcacdf148ca30f
sosow0212 <sosow0212@naver.com>
2022-01-16 13:20:49 +0900

    Item Service & Controller - 2

--- src/main/java/com/example/shoppingmall/web/ItemController.java
+++ src/main/java/com/example/shoppingmall/web/ItemController.java
@@ -1,11 +1,13 @@
 package com.example.shoppingmall.web;
 
+import com.example.shoppingmall.domain.item.Item;
 import com.example.shoppingmall.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -25,6 +27,41 @@ public class ItemController {
     public String itemView(@PathVariable("id") Integer id, Model model) {
         model.addAttribute("item", itemService.itemView(id));
         return "item";
+    }
+
+    // 아이템 업로드 페이지
+    @GetMapping("/item/upload")
+    public String itemUpload() {
+        return "itemupload";
+    }
+
+
+    // 아이템 업로드 진행
+    @PostMapping("/item/upload/process")
+    public String itemUploadProcess(Item item) {
+        itemService.saveItem(item);
+        return "redirect:/main";
+    }
+
+
+    // 아이템 수정 페이지
+    @GetMapping("/item/{id}/modify")
+    public String itemModify(@PathVariable("id") Integer id) {
+        return "itemmodify";
+    }
+
+
+    // 아이템 수정 처리
+    @PostMapping("/item/{id}/modify/process")
+    public String itemModifyProcess(Item item, @PathVariable("id") Integer id) {
+        return "redirect:/main";
+    }
+
+    // 아이템 삭제
+    @GetMapping("/item/{id}/delete")
+    public String itemDelete(@PathVariable("id") Integer id) {
+        itemService.itemDelete(id);
+        return "main";
     }
 
 
