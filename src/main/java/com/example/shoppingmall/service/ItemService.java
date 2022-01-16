fdd11bc302e96aa1aae365547475472622dd418e
sosow0212 <sosow0212@naver.com>
2022-01-16 13:10:36 +0900

    Item Service & Controller - 1

--- src/main/java/com/example/shoppingmall/service/ItemService.java
+++ src/main/java/com/example/shoppingmall/service/ItemService.java
@@ -1,4 +1,37 @@
 package com.example.shoppingmall.service;
 
+import com.example.shoppingmall.domain.item.Item;
+import com.example.shoppingmall.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class ItemService {
+    private final ItemRepository itemRepository;
+
+    // 아이템 등록
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+
+    // 아이템 리스트 불러오기
+    public List<Item> allItemView() {
+        return itemRepository.findAll();
+    }
+
+    // 아이템 개별로 불러오기
+    public Item itemView(Integer id) {
+        return itemRepository.findById(id).get();
+    }
+
+
+    // 아이템 삭제
+    public void itemDelete(Integer id) {
+        itemRepository.deleteById(id);
+    }
+
 }
--- src/main/java/com/example/shoppingmall/web/ItemController.java
+++ src/main/java/com/example/shoppingmall/web/ItemController.java
@@ -1,4 +1,31 @@
 package com.example.shoppingmall.web;
 
+import com.example.shoppingmall.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
 public class ItemController {
+
+    private static ItemService itemService;
+
+    // 메인 페이지
+    @GetMapping({"/", "item"})
+    public String mainPage(Model model) {
+        model.addAttribute("items", itemService.allItemView());
+        return "main";
+    }
+
+    // 아이템 상세 페이지
+    @GetMapping("/item/{id}")
+    public String itemView(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("item", itemService.itemView(id));
+        return "item";
+    }
+
+
 }
