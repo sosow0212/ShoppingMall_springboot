de0a56c3463d7aa8dd54db96cea0d686b4035a14
sosow0212 <sosow0212@naver.com>
2022-01-16 22:10:16 +0900

    유저 페이지 장바구니 렌더링 - 1

--- src/main/java/com/example/shoppingmall/web/UserPageController.java
+++ src/main/java/com/example/shoppingmall/web/UserPageController.java
@@ -16,8 +16,28 @@ public class UserPageController {
 
     // 유저 페이지
     @GetMapping("/user/{id}")
-    public String userPage(@PathVariable("id") Integer id, Model model) {
-        model.addAttribute("user", userPageService.findUser(id));
-        return "/user/userPage";
+    public String userPage(@PathVariable("id") Integer id, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        if (principalDetails.getUser().getId() == id) {
+            // 로그인 정보와 접속하는 유저 페이지의 id 값이 같으면 유저페이지 렌더링
+            // 즉 본인은 본인 페이지만 볼 수 있음
+            model.addAttribute("user", userPageService.findUser(id));
+            return "/user/userPage";
+        } else {
+            return "redirect:/main";
+        }
+
+    }
+
+    // 장바구니
+    @GetMapping("/user/{id}/cart")
+    public String userCart(@PathVariable("id") Integer id, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        if (principalDetails.getUser().getId() == id) {
+            // 로그인 정보와 접속하는 유저 페이지의 id 값이 같으면 유저페이지 렌더링
+            // 즉 본인은 본인 페이지만 볼 수 있음
+            model.addAttribute("user", userPageService.findUser(id));
+            return "/user/userCart";
+        } else {
+            return "redirect:/main";
+        }
     }
 }
